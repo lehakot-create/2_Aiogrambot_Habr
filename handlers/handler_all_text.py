faee9e81b80e35392cb639a4ba0c5fc4fc597cbb
lehakot-create <lehakot85@inbox.ru>
2023-03-25 16:44:14 +0400

    подключены новости

--- handlers/handler_all_text.py
+++ handlers/handler_all_text.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 
 from handlers.handler import Handler
+from utils.parser_habr_news import get_habr_news
 
 
 class HandlerAllText(Handler):
@@ -27,12 +28,22 @@ class HandlerAllText(Handler):
                                     user_subscribe
                                  ))
 
+    async def get_my_news(self, message: Message):
+        my_themes = self.DB.get_user_subscribe(message.from_user.id)
+        for theme in my_themes:
+            print(theme.id, theme.title, theme.url)
+            my_news = get_habr_news(url_theme=theme.url)
+            for el in my_news:
+                await message.answer(f"{el.get('title')}\n https://habr.com{el.get('url')}",
+                                     parse_mode='HTML')
+
     def handle(self):
         @self.router.message(F.text)
         async def get_news(message: Message):
             if message.text == 'Получить новости':
-                await message.answer('Раздел новостей в разработке',
-                                     reply_markup=self.kb.main_kb())
+                await self.get_my_news(message)
+                # await message.answer('Раздел новостей в разработке',
+                #                      reply_markup=self.kb.main_kb())
             elif message.text == 'Все темы':
                 await message.answer('Доступные темы',
                                      reply_markup=self.inline_kb.inline_list_themes(
--- utils/parser_habr_news.py
+++ utils/parser_habr_news.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 def get_habr_news(url_theme: str) -> dict:
     habr_url = 'https://habr.com' + url_theme
+    lst = []
 
     req = requests.get(habr_url)
 
@@ -13,8 +14,8 @@ def get_habr_news(url_theme: str) -> dict:
     for article in all_articles[:3]:
         url = article.find('a', class_='tm-article-snippet__title-link').get('href')
         title = article.find('a', class_='tm-article-snippet__title-link').find('span').text
-
-    return {'url': url, 'title': title}
+        lst.append({'url': url, 'title': title})
+    return lst
 
 
 # /ru/hub/programming/
